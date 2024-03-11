ec60874bbd986da8f75bc78501419c71ee894cb7
Joshi <anna4joshi@hotmail.com>
2024-03-11 04:04:00 -0700

    first commit

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h3>Welcome to React-App</h3>
+      <img className="home-img" src="istockphoto-1363104923-612x612.jpg" alt="Welcome picture" />
+    </div>
+  )
+}
---
+++ src/global.js
@@ -0,0 +1,1 @@
+export const API = "https://resumebuilder-backend.vercel.app"
